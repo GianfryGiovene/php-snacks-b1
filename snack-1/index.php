8346a3c06c94bc5cccb89b3317a0b6121b67aba5
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-03 13:49:21 +0200

    first snack done

--- snack-1/index.php
+++ snack-1/index.php
@@ -4,3 +4,31 @@
     punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
     Olimpia Milano - Cantù | 55-60
 -->
+
+<?php 
+    $matches = [
+        [
+            'home'=> 'Sardine',
+            'guest' => 'Palombari',
+            'homeScore' => 12,
+            'guestScore' => 22
+        ],
+        [
+            'home'=> 'Pepe',
+            'guest' => 'Sale',
+            'homeScore' => 22,
+            'guestScore' => 34
+        ],
+        [
+            'home'=> 'Ketchup',
+            'guest' => 'Maionese',
+            'homeScore' => 1,
+            'guestScore' => 55
+        ]
+    ];
+
+    for($i = 0; $i < count($matches); $i++){
+        $match = $matches[$i];
+        echo ("<p> {$match["home"]} -  {$match["guest"]} | {$match["homeScore"]}-{$match["guestScore"]}</p>");
+    }
+?>
