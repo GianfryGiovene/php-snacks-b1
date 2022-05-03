ae11b647c6a8a28997ee4c1b2999a153a8eb8e3c
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-03 14:35:50 +0200

    snack 4 done

--- snack-4/index.php
+++ snack-4/index.php
@@ -2,3 +2,15 @@
     Snack 4
     Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
 -->
+
+<?php
+
+    $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Aut eligendi commodi, minima velit eum aliquam odit dolorem provident quos. 
+        Repudiandae, modi suscipit repellat odio aliquid magnam. Doloremque temporibus animi facilis?";
+
+
+    $manyParagraph = explode('.', $paragraph);
+    
+    var_dump($manyParagraph);
+?>
